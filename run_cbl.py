b69d7e294cd43c81f9564bdedf69354fc48d8cd0
Igor Martayan <igor@martayan.org>
2024-02-07 16:48:58 +0100

    Update run_cbl

--- run_cbl.py
+++ run_cbl.py
@@ -17,7 +17,7 @@ def build(fasta_file, **params):
         f"cd CBL && K={k} PREFIX_BITS={prefix_bits} cargo +nightly build --release --examples"
     )
     return measure_time(
-        f"./CBL/target/release/examples/build_index {fasta_file} -o {cbl_file}",
+        f"./CBL/target/release/examples/cbl build {fasta_file} -o {cbl_file}",
         **params
     )
 
@@ -26,7 +26,7 @@ def count(indexed_file, **params):
     prefix = f"{OUT_FOLDER}/{get_basename(indexed_file)}"
     cbl_file = prefix + ".cbl"
     out, _ = run_cmd(
-        f"./CBL/target/release/examples/index_count {cbl_file} 2>&1 | tail -n 1 | cut -d ' ' -f 3",
+        f"./CBL/target/release/examples/cbl count {cbl_file} 2>&1 | tail -n 1 | cut -d ' ' -f 3",
         **params
     )
     return int(out)
@@ -36,60 +36,7 @@ def count_query(indexed_file, query_file, **params):
     prefix = f"{OUT_FOLDER}/{get_basename(indexed_file)}"
     cbl_file = prefix + ".cbl"
     out, _ = run_cmd(
-        f"./CBL/target/release/examples/query_index {cbl_file} {query_file} 2>&1 | tail -n 2 | head -n 1 | cut -d ' ' -f 3",
+        f"./CBL/target/release/examples/cbl query {cbl_file} {query_file} 2>&1 | tail -n 2 | head -n 1 | cut -d ' ' -f 3",
         **params
     )
     return int(out)
-
-
-def query(indexed_file, query_file, **params):
-    prefix = f"{OUT_FOLDER}/{get_basename(indexed_file)}"
-    cbl_file = prefix + ".cbl"
-    return measure_time(
-        f"./CBL/target/release/examples/query_index {cbl_file} {query_file}",
-        **params
-    )
-
-
-def insert(indexed_file, query_file, **params):
-    prefix = f"{OUT_FOLDER}/{get_basename(indexed_file)}"
-    cbl_file = prefix + ".cbl"
-    updated_file = f"{prefix}_add_{get_basename(query_file)}.cbl"
-    return measure_time(
-        f"./CBL/target/release/examples/insert_index {cbl_file} {query_file} -o {updated_file}",
-        **params
-    )
-
-
-def remove(indexed_file, query_file, **params):
-    prefix = f"{OUT_FOLDER}/{get_basename(indexed_file)}"
-    cbl_file = prefix + ".cbl"
-    updated_file = f"{prefix}_rem_{get_basename(query_file)}.cbl"
-    return measure_time(
-        f"./CBL/target/release/examples/remove_index {cbl_file} {query_file} -o {updated_file}",
-        **params
-    )
-
-
-def merge(indexed_file, other_indexed_file, **params):
-    prefix = f"{OUT_FOLDER}/{get_basename(indexed_file)}"
-    cbl_file = prefix + ".cbl"
-    other_prefix = f"{OUT_FOLDER}/{get_basename(other_indexed_file)}"
-    other_cbl_file = other_prefix + ".cbl"
-    updated_file = f"{prefix}_uni_{get_basename(other_indexed_file)}.cbl"
-    return measure_time(
-        f"./CBL/target/release/examples/merge_index {cbl_file} {other_cbl_file} -o {updated_file}",
-        **params
-    )
-
-
-def intersect(indexed_file, other_indexed_file, **params):
-    prefix = f"{OUT_FOLDER}/{get_basename(indexed_file)}"
-    cbl_file = prefix + ".cbl"
-    other_prefix = f"{OUT_FOLDER}/{get_basename(other_indexed_file)}"
-    other_cbl_file = other_prefix + ".cbl"
-    updated_file = f"{prefix}_int_{get_basename(other_indexed_file)}.cbl"
-    return measure_time(
-        f"./CBL/target/release/examples/intersect_index {cbl_file} {other_cbl_file} -o {updated_file}",
-        **params
-    )
