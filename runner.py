7b2d13d45d003cb25b0184a2a5b70fbabd2566d2
Igor Martayan <igor@martayan.org>
2024-02-12 15:45:52 +0100

    Update tools in runner

--- runner.py
+++ runner.py
@@ -1,19 +1,23 @@
-from util import get_basename, get_filesize
+from util import get_basename, get_filesize, run_cmd
 import os
 import json
 import run_bifrost
+import run_brisk
 import run_cbl
 import run_hashmap
+import run_jellyfish
 
 
 DATA_FOLDER = "data"
 CLI = {
     "Bifrost": run_bifrost,
+    "Brisk": run_brisk,
     "CBL": run_cbl,
     "HashMap": run_hashmap,
+    "Jellyfish": run_jellyfish,
 }
 TOOLS = {
-    "build": ["CBL", "HashMap", "Bifrost"],
+    "build": ["Brisk", "Jellyfish", "HashMap", "Bifrost", "CBL"],
 }
 
 
@@ -40,17 +44,29 @@ def update_data(json_file, field=None, **kwargs):
             json.dump(data, f)
 
 
+def build_filename(prefix, fasta_file, **params):
+    k = params["k"]
+    m = params["m"]
+    basename = get_basename(fasta_file)
+    size = get_filesize(fasta_file)
+    return f"{DATA_FOLDER}/{prefix}_{k}_{m}_{size}_{basename}.json"
+
+
 def build(fasta_file, **params):
     output = build_filename("build", fasta_file, **params)
+    run_cmd(f"cd kmer_count && K={params['k']} cargo build --release")
+    out, _ = run_cmd(f"./kmer_count/target/release/kmer_count {fasta_file}")
     update_data(
         output,
         file=fasta_file,
         bytes=get_filesize(fasta_file),
         k=params["k"],
+        m=params["m"],
+        b=params["b"],
         prefix_bits=params["prefix_bits"],
+        kmers=int(out),
     )
     for tool in TOOLS["build"]:
         time, mem = CLI[tool].build(fasta_file, **params)
         if time and mem:
             update_data(output, field=tool, time=time, mem=mem)
-    update_data(output, kmers=run_cbl.count(fasta_file, **params))
