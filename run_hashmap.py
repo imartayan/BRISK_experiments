8220ff0efd99a4cddd58dcc526868e92bca969b2
Igor Martayan <igor@martayan.org>
2024-02-12 15:44:36 +0100

    Remove nightly for hashmap

--- run_hashmap.py
+++ run_hashmap.py
@@ -7,7 +7,7 @@ def build(fasta_file, **params):
     os.makedirs(OUT_FOLDER, exist_ok=True)
     prefix = f"{OUT_FOLDER}/{get_basename(fasta_file)}"
     hash_file = prefix + ".hash"
-    run_cmd(f"cd hashmap && K={k} cargo +nightly build --release")
+    run_cmd(f"cd hashmap && K={k} cargo build --release")
     return measure_time(
         f"./hashmap/target/release/hashmap build {fasta_file} -o {hash_file}", **params
     )
