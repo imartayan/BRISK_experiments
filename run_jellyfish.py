47ddf9b557454b85a938a2de2dd74fa10f05c8a4
Igor Martayan <igor@martayan.org>
2024-02-12 14:17:02 +0100

    Accurate build time for jellyfish

--- run_jellyfish.py
+++ run_jellyfish.py
@@ -6,7 +6,10 @@ def build(fasta_file, **params):
     k = params["k"]
     s = params["jellyfish_size"]
     threads = params["threads"]
-    return measure_time(
+    _, mem, out, _ = measure_time_output(
         f"./jellyfish/bin/jellyfish count -m {k} -s {s} -t {threads} -C {fasta_file} --timing=/dev/stdout -o /dev/null",
         **params
     )
+    t0 = float(out.splitlines()[0].split()[-1].strip())
+    t1 = float(out.splitlines()[1].split()[-1].strip())
+    return t0 + t1, mem
