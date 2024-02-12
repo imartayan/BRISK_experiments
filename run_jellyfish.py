17135a39b4f77331f6bee3c45801405093ea3b6b
Igor Martayan <igor@martayan.org>
2024-02-12 15:44:54 +0100

    Unzip file before running jellyfish

--- run_jellyfish.py
+++ run_jellyfish.py
@@ -6,6 +6,11 @@ def build(fasta_file, **params):
     k = params["k"]
     s = params["jellyfish_size"]
     threads = params["threads"]
+    if fasta_file.endswith(".gz"):
+        os.makedirs(OUT_FOLDER, exist_ok=True)
+        unzipped_file = f"{OUT_FOLDER}/{get_basename(fasta_file)}"
+        run_cmd(f"gzip -cd {fasta_file} > {unzipped_file}")
+        fasta_file = unzipped_file
     _, mem, out, _ = measure_time_output(
         f"./jellyfish/bin/jellyfish count -m {k} -s {s} -t {threads} -C {fasta_file} --timing=/dev/stdout -o /dev/null",
         **params
