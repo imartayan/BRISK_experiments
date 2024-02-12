5fbf31675d492487ce7d264eeee858fe273d74c3
Igor Martayan <igor@martayan.org>
2024-02-12 14:16:47 +0100

    Accurate build time for brisk

--- run_brisk.py
+++ run_brisk.py
@@ -7,7 +7,9 @@ def build(fasta_file, **params):
     m = params["m"]
     b = params["b"]
     threads = params["threads"]
-    return measure_time(
+    _, mem, out, _ = measure_time_output(
         f"./Brisk/counter -f {fasta_file} -k {k} -m {m} -b {b} -t {threads} --mode 1",
         **params
     )
+    time = float(out.splitlines()[4].split(":")[1].strip()[:-1])
+    return time, mem
