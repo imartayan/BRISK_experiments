c8940865682069c79756f32262e91735dbb8d23a
Igor Martayan <igor@martayan.org>
2024-02-12 14:51:10 +0100

    Remove serialization in run_cbl

--- run_cbl.py
+++ run_cbl.py
@@ -2,44 +2,16 @@ from util import *
 import os
 
 
-def index_path(fasta_file):
-    prefix = f"{OUT_FOLDER}/{get_basename(fasta_file)}"
-    return prefix + ".cbl"
-
-
 def build(fasta_file, **params):
     k = params["k"]
     prefix_bits = params["prefix_bits"]
     threads = params["threads"]
     if k > 59 or threads > 1:
         return None, None
-    os.makedirs(OUT_FOLDER, exist_ok=True)
-    prefix = f"{OUT_FOLDER}/{get_basename(fasta_file)}"
-    cbl_file = prefix + ".cbl"
     run_cmd(
         f"cd CBL && K={k} PREFIX_BITS={prefix_bits} cargo +nightly build --release --examples"
     )
     return measure_time(
-        f"./CBL/target/release/examples/cbl build {fasta_file} -o {cbl_file}",
+        f"./CBL/target/release/examples/cbl build {fasta_file}",
         **params
     )
-
-
-def count(indexed_file, **params):
-    prefix = f"{OUT_FOLDER}/{get_basename(indexed_file)}"
-    cbl_file = prefix + ".cbl"
-    out, _ = run_cmd(
-        f"./CBL/target/release/examples/cbl count {cbl_file} 2>&1 | tail -n 1 | cut -d ' ' -f 3",
-        **params
-    )
-    return int(out)
-
-
-def count_query(indexed_file, query_file, **params):
-    prefix = f"{OUT_FOLDER}/{get_basename(indexed_file)}"
-    cbl_file = prefix + ".cbl"
-    out, _ = run_cmd(
-        f"./CBL/target/release/examples/cbl query {cbl_file} {query_file} 2>&1 | tail -n 2 | head -n 1 | cut -d ' ' -f 3",
-        **params
-    )
-    return int(out)
