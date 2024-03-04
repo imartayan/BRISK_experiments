73d7587623a202dd976253927f12dc440871fcdd
Igor Martayan <igor@martayan.org>
2024-03-04 16:48:46 +0100

    First Rust compilation in the install script

--- install_all.sh
+++ install_all.sh
@@ -6,3 +6,15 @@ bash install_apt_dependencies.sh
 bash install_bifrost.sh
 bash install_brisk.sh
 bash install_jellyfish.sh
+
+cd kmer_count
+cargo build --release
+cd ..
+
+cd hashmap
+cargo build --release
+cd ..
+
+cd CBL
+cargo +nightly build --release --examples
+cd ..
